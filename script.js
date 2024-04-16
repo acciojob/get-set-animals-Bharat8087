4905ac4b46bb6155e88681308bf46e97962c139f
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-16 20:14:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //complete this code
-class Animal {}
+class Animal {
+	private species : string;
+	constructor(species:string){
+		this.species = species;
+	}
+	 get species(): string {
+    return this.species;
+  }
+
+  makeSound(): void {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+class Cat extends Animal {
+  purr(): void {
+    console.log("purr");
+  }
+}
+class Dog extends Animal {
+  bark(): void {
+    console.log("woof");
+  }
+
+}
 
 class Dog extends Animal {}
 
